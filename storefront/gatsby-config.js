d4611535cdade9ce448b6cc4638d43d6921325b4
Noah <admin@noahschepers.com>
2022-08-06 17:38:01 -0400

    alter siteMetadata

--- storefront/gatsby-config.js
+++ storefront/gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Medusa Gatsby Starter`,
-    description: `Kick off your next, great e-commerce project with this default starter with Medusa, Gatsby and common dev tools.`,
-    author: `@medusajs`,
+    title: `Silly Goose`,
+    description: `Just some silly geese`,
+    author: `@sch3p`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
